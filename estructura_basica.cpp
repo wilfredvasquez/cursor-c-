02c470f838c91fc6270ef2374c7fa41799a77049
Wilfred Vásquez <wilfredvas@gmail.com>
2024-07-31 08:14:42 -0400

    Basic structure

---
+++ estructura_basica.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+/*
+Este programa fue hecho como ejemplo para Código que Transforma
+Su autor es Wilfred Vásquez
+*/
+
+int main() {
+    // Aquí se inicia el programa
+    cout << "Hola mundo" << endl;
+    cout << "Texto de ejemplo";
+    return 0;
+}
