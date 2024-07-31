4b52c19cf98aa9806bd36ddd668dc7754e2178ae
Wilfred Vásquez <wilfredvas@gmail.com>
2024-07-31 08:17:45 -0400

    Variables and types

---
+++ tipos_variables.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Entero
+    int num = 3;
+
+    // Decimales
+    float temp = 5.6;
+    double radio = 5.248;
+
+    // Caracter
+    char letra = 'C';
+    // Cadena de texto
+    string texto = "Esto es un texto";
+
+    // Booleano
+    bool verdadero = true;
+
+    // Constante Decimal
+    const float PI = 3.1416;
+
+    cout << num;
+    return 0;
+}
